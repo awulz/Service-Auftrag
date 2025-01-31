a0c09c52e9e09aee6ba7eb6d28145cea583e3cae
tk373 <silva.hegner@gmail.com>
2025-02-01 00:35:02 +0100

    project params added

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Service Auftrag
+
+## Backend 
+
+### Dev Server 
+
+Kann gestartet werden mit `php -S localhost:8000`
+
+
+## Frontend
+
+### React-app 
+
+Zuerst nodes modules installieren mit `npm i`
+
+Dann dev server starten mit `npm run dev`
+
+
+## Datenbank
+
+MySQL Datenbank kann über Linux/Unix mit homebrew installiert und gestartet werden, bei windows ist es noch einfacher.
+
+Linux/Unix: `brew services start mysql`
--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,4 @@
 <?php
-// index.php
 $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method  = $_SERVER['REQUEST_METHOD'];
 
@@ -8,6 +7,50 @@ if ($request === '/api/hello' && $method === 'GET') {
     echo json_encode(['message' => 'Hello, World!']);
     exit;
 }
+
+if ($request === '/login' && $method === 'POST') {
+    header('Content-Type: application/json');
+    //Logik muss noch implementiert werden.
+    echo json_encode(['message' => 'Login endpoint - not implemented yet']);
+    exit;
+}
+
+if ($request === '/register' && $method === 'POST') {
+    header('Content-Type: application/json');
+    //^^
+    echo json_encode(['message' => 'Registration endpoint - not implemented yet']);
+    exit;
+}
+
+if ($request === '/logout' && $method === 'POST') {
+    header('Content-Type: application/json');
+    //^^
+    echo json_encode(['message' => 'Registration endpoint - not implemented yet']);
+    exit;
+}
+
+if ($request === '/getDataWhatever' && $method === 'POST') {
+    header('Content-Type: application/json');
+    //^^
+    echo json_encode(['message' => 'Registration endpoint - not implemented yet']);
+    exit;
+}
+
+if ($request === '/writeDataWhatever' && $method === 'POST') {
+    header('Content-Type: application/json');
+    //^^
+    echo json_encode(['message' => 'Registration endpoint - not implemented yet']);
+    exit;
+}
+
+if ($request === '/updateDataWhatever' && $method === 'POST') {
+    header('Content-Type: application/json');
+    //^^
+    echo json_encode(['message' => 'Registration endpoint - not implemented yet']);
+    exit;
+}
+
+//wenn noch gebraucht, dann User-management also CRUD für User (relevant für admin)
 
 http_response_code(404);
 echo json_encode(['error' => 'Endpoint not found']);
